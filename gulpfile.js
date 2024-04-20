6343d99075ad4b21a1db07471a93d051b5ebc220
xuliangzhan <xu_liangzhan@163.com>
2024-04-20 13:03:45 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const exportModuleName = 'XEClipboard'
 
 gulp.task('build_commonjs', function () {
   return gulp.src(['index.ts'])
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(ts({
       strict: true,
       moduleResolution: 'node',
@@ -26,7 +26,7 @@ gulp.task('build_commonjs', function () {
       basename: pack.name,
       extname: '.common.js'
     }))
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'))
 })
 
